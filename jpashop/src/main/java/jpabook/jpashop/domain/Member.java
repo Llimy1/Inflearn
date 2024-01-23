e440b28dbfd2bf40b7a801bdaa8474b797798f96
Llimy1 <alsgur990104@gmail.com>
2024-01-23 14:37:44 +0900

    :sparkles: 영한쌤 JPA2 (회원 API)

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,11 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
